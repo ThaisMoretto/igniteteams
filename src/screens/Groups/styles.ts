59e6b4687996ac5120d5667ed6bb83f01b98d98d
Thais Helena Moretto <CIANDT\thaism@lnb028293cps.ciandt.global>
2023-05-01 10:19:02 -0300

    Adiciona styled-components

---
+++ src/screens/Groups/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+  flex: 1;
+  background-color: #fff;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Title = styled.Text`
+  color: "FFF";
+  font-size: 32px;
+`;
+
+// export const Container = styled.View``;
+
+// export const Container = styled.View``;
