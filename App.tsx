50b11653fd8ac17e875f5b163921f5ea0ce3c451
Thais Helena Moretto <CIANDT\thaism@lnb028293cps.ciandt.global>
2023-07-31 20:01:11 -0300

    Adição da lib react-navigation e definicao da estrategia de rotas em stack

--- App.tsx
+++ App.tsx
@@ -6,10 +6,11 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 
+import { Loading } from "@components/Loading";
+
 import theme from "./src/theme";
 
-import { Loading } from "@components/Loading";
-import { Groups } from "@screens/Groups";
+import { Routes } from "./src/routes";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -21,7 +22,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <Routes /> : <Loading />}
     </ThemeProvider>
   );
 }
