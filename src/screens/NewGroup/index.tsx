f386b2eb560116bcd842237a9f2c788124111d5c
Thais Helena Moretto <CIANDT\thaism@lnb028293cps.ciandt.global>
2023-08-01 20:18:47 -0300

    Utilizando useNavigation e SafeAreaView

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+
 import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
@@ -5,6 +8,13 @@ import { Input } from "@components/Input";
 import { Container, Content, Icon } from "./styles";
 
 export function NewGroup() {
+  const [group, setGroup] = useState("");
+  const navigation = useNavigation();
+
+  function handleNew() {
+    navigation.navigate("players", { group });
+  }
+
   return (
     <Container>
       <Header showBackButton />
@@ -15,8 +25,8 @@ export function NewGroup() {
           title="Nova turma"
           subtitle="Crie a turma para adicionar as pessoas"
         />
-        <Input placeholder="Nome da turma" />
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Input placeholder="Nome da turma" onChangeText={setGroup} />
+        <Button title="Criar" style={{ marginTop: 20 }} onPress={handleNew} />
       </Content>
     </Container>
   );
